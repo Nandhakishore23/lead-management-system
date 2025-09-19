507b0faeaf5cc28fe80ee3a2d75f1878b9b18e8c
Nandhakishore A <nandha18kishore@gmail.com>
2025-09-19 16:24:45 +0530

    updated api

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,9 +15,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 // CORS - allow frontend origin to send credentials
-// const FRONTEND_URL = process.env.FRONTEND_URL ;
+const FRONTEND_URL = "https://lead-management-system-silk.vercel.app";
 app.use(cors({
-  origin: '*',
+  origin: FRONTEND_URL,
   credentials: true
 }));
 
